307e46e44a7e7e1a1d0e1ba786e89bfab05910f3
Paul Whurr <pawhurr@gmail.com>
2019-04-29 09:48:36 -0400

    added link to liri and node word guess

---
+++ script.js
@@ -0,0 +1,18 @@
+var links = [
+    "https://pawhurr.github.io/TriviaGame/", 
+    "https://pawhurr.github.io/Unit-4-Game/", 
+    "https://pawhurr.github.io/Project1-DataBite/",
+    "https://pawhurr.github.io/Psychic-Game/",
+    "https://pawhurr.github.io/Giphy/",
+    "https://pawhurr.github.io/Train-Scheduler/"]
+var source = [ 
+    "images/the_office_logo.jpg",
+    "images/redcrystal.jpg",
+    "images/transparent-logo2.png",
+    "http://lorempixel.com/output/abstract-q-c-640-480-1.jpg",
+    "images/Title.png",
+    "images/train.png"]
+
+    for (var i = 0; i < links.length; i++) {
+        
+    }
